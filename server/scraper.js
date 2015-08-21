0c06d912f716b9f69b7998e39c7c12c4fab1d35a
ephetic <bpcleary@gmail.com>
2015-08-21 13:55:04 -0700

    [refactor] combines servers into single worker

---
+++ server/scraper.js
@@ -0,0 +1,64 @@
+var request = require('request');
+var Firebase = require('firebase');
+var fb_link = require('../firebaselink');
+
+var latestUrl = 'https://parking.api.smgov.net/meters/events/latest';
+var ordinalUrl = 'https://parking.api.smgov.net/meters/events/since/';
+
+var ordinalNumber = 0;
+var done = false;
+var numCalls = 0;
+
+var fb = new Firebase(fb_link.url);
+
+//fetch the latest ordinal number
+var getOrdinalNumber = function() {
+  request(latestUrl, function (error, response, body) {
+    if (error) { console.log('error while fetching', error); }
+    if (!error && response.statusCode === 200) {
+      var results = JSON.parse(body);
+      //console.log('Fetched Ordinal Number:');
+      //console.log(results);
+      ordinalNumber = results.ordinal;
+      requestApiEvents();
+    }
+    else {
+      getOrdinalNumber();
+    }
+  });
+}
+
+  //request event information from the sm api
+var requestApiEvents = function() {
+  request(ordinalUrl+ordinalNumber, function (error, response, body) {
+    if (error) { console.log('error while fetching', error); }
+    if (!error && response.statusCode === 200) {
+      var results = JSON.parse(body);
+
+      for(var i=0; i<results.length; i++) {
+        fb.child('MeteredParkingSpots').child(results[i].meter_id).child('mostRecentEvent').set(results[i].event_type);
+        fb.child('MeteredParkingSpots').child(results[i].meter_id).child('timeStamp').set(results[i].event_time);
+      }  
+
+      ordinalNumber = results[0].ordinal;
+
+      //log calls
+      console.log('Number of Calls:',++numCalls);
+      console.log('Number of park events: ',results.length);
+      console.log('Ordinal Number:', ordinalNumber);
+      console.log('****************logging data***************');
+    }
+    if(!error) {
+      console.log('statusCode:', response.statusCode);
+    }
+    setTimeout(requestApiEvents, 10000);
+  });
+};  //requestApiEvents ends here
+
+// //to only call requestApiEvents once after it is running
+if (!done) {
+  getOrdinalNumber();
+  done = true;
+};
+
+module.exports = {};
--- server/server.js
+++ server/server.js
@@ -1,41 +1,4 @@
-var Firebase = require('firebase');
-var fb_keys = require('../firebaselink.js');
+var scraper = require('./scraper');
+var recommender = require('./recommender');
 
-
-var distance = function(latU, longU, latP, longP) {
-  return Math.sqrt(Math.pow((latP - latU) * 69.1128, 2) + Math.pow((longP - longU) * 57.2807, 2));
-}
-
-var firecloud = new Firebase(fb_keys.url);
-firecloud.child('Users').remove();
-var usersRef = firecloud.child('Users'); 
-
-usersRef.on('child_added', function(childSnapshot, prevChildKey) {
-  var start = Date.now();
-  var user = childSnapshot.val();
-  var userKey = childSnapshot.key();
-  console.log('+', userKey);
-
-  var radius = user.range;
-  var tuple = [user.latitude, user.longitude];
-
-  firecloud.child('MeteredParkingSpots').once('value', function(snapshot) {
-
-    var pSpots = snapshot.val();
-    var closeSpots = [];
-    var freeSpots = {};
-
-    for (var key in pSpots) {
-      var displacement = distance(tuple[0], tuple[1], pSpots[key].latitude, pSpots[key].longitude);
-      if (displacement < radius) {
-
-        pSpots[key].distance = displacement;
-        if (pSpots[key].mostRecentEvent === 'SE') {
-          freeSpots[key] = pSpots[key];
-        }
-      } 
-    } 
-    firecloud.child('Users').child(userKey).child('Recommendations').set(freeSpots);
-    console.log('-', userKey, Date.now() - start, 'ms');
-  });
-});
+console.log('started worker');
